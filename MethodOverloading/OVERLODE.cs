4780ec2420c102e0657c4074159614571d9321ce
Jorge Saldana <jojonoob134@gmail.com>
2023-08-31 11:39:06 -0500

    made overlode methods

---
+++ MethodOverloading/OVERLODE.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class OVERLODE
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Add(bool money, int a, int b)
+        {
+            int total = a + b;
+            if (money == true && total <= 1)
+            {
+                Console.WriteLine($"you have {total} dollar in total");
+            }
+            else if (money == true)
+            {
+                Console.WriteLine($"you have {total} dollars in total");
+            }
+            else
+            {
+                Console.WriteLine("you have no dollars");
+            }
+        }
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            OVERLODE.Add(4,3);
+            OVERLODE.Add(4.6, 3.9);
+            Console.WriteLine("how much money did you have");
+            int yestMon = int.Parse(Console.ReadLine());
+            Console.WriteLine("how much money do you have");
+            int todayMon = int.Parse(Console.ReadLine());
+            OVERLODE.Add(true, yestMon, todayMon);
         }
     }
 }
